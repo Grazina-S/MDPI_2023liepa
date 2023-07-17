c3ab2cea86422606f966d197a283db81e7caddc7
Grazina-S <grazina.statkeviciute@gmail.com>
2023-07-17 21:36:35 +0300

    skriptas

--- 02_r_scripts/Ugne.R
+++ 02_r_scripts/Ugne.R
@@ -7,3 +7,8 @@ library(readxl)
 library(openxlsx)
 
 # Nuskaitymas is excel #########################
+dmy <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "DMY")
+scores <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "scores")
+quality <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "quality")
+file.path <- "01_tidy_data/dmy.csv"
+write.csv(dmy, file = file.path, row.names = FALSE)

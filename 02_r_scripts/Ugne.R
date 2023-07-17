6d87ffaaac8db3cd794a3ee69ccb1de7dd2fbad0
Grazina-S <grazina.statkeviciute@gmail.com>
2023-07-17 21:35:08 +0300

    DMY failas

---
+++ 02_r_scripts/Ugne.R
@@ -0,0 +1,9 @@
+# Reikalingi packages #####
+install.packages("readxl")
+install.packages("writexl")
+
+# #########################
+library(readxl)
+library(openxlsx)
+
+# Nuskaitymas is excel #########################

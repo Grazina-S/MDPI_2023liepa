f92b6345b8247f95658d39bef610a5e5a86024ff
Grazina-S <grazina.statkeviciute@gmail.com>
2023-07-18 17:16:02 +0300

    biski bendros statistikos

--- 02_r_scripts/Ugne.R
+++ 02_r_scripts/Ugne.R
@@ -3,12 +3,17 @@ install.packages("readxl")
 install.packages("writexl")
 install.packages('agricolae')
 install.packages("metan")
+install.packages("toolStability")
 
 # laibrariai #########################
 library(readxl)
 library(openxlsx)
 library(agricolae)
 library(metan)
+library(tidyverse)
+library(toolStability)
+library(rstatix)
+
 # Nuskaitymas is excel #########################
 dmy <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "DMY")
 scores <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "scores")
@@ -26,6 +31,11 @@ write.csv(quality, file = path.quality, row.names = F)
 dmy <- dmy[-c(37, 44), ] # pasalinau elutes 37 ir 44 nes buvo tuscios
 dmy$year <- as.factor(dmy$year)
 dmy$use_year <- as.factor(dmy$use_year)
-dmy$total <- rowSums(dmy[, 3:6]) # padarau nauja stulpeli su visu metu derlium
+dmy$total <- rowSums(dmy[, 3:6], na.rm = TRUE) # padarau nauja stulpeli su visu metu derlium
 dmy[, 3:7] <- round(dmy[, 3:7], 2) # suapvalinau stulpelius 3-7 iki 2 vietu po kablelio
+dmy %>% group_by(year, use_year) %>% get_summary_stats(cut1, cut2, cut3, cut4, total,
+                                                       show = c("mean", "sd", "se"))
+# Basic statistika ##### 
+dmy_stats <- dmy %>% group_by(year, use_year) %>% get_summary_stats(cut1, cut2, cut3, cut4, total,
+                                                                    show = c("mean", "sd", "se"))
 

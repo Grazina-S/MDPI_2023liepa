6403a2724c4e56d32ffdbec2e97c054b7c94dcd1
Grazina-S <grazina.statkeviciute@gmail.com>
2023-07-18 14:03:34 +0300

    csv failai

--- 02_r_scripts/Ugne.R
+++ 02_r_scripts/Ugne.R
@@ -1,14 +1,31 @@
 # Reikalingi packages #####
 install.packages("readxl")
 install.packages("writexl")
+install.packages('agricolae')
+install.packages("metan")
 
-# #########################
+# laibrariai #########################
 library(readxl)
 library(openxlsx)
-
+library(agricolae)
+library(metan)
 # Nuskaitymas is excel #########################
 dmy <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "DMY")
 scores <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "scores")
 quality <- read_excel("00_raw_data\\Ugnė_nuo 2009.xlsx", sheet = "quality")
-file.path <- "01_tidy_data/dmy.csv"
+#          perkelimas i csv
+file.path <- "01_tidy_data/dmy.csv"            # Kad atsidurtu subfolderyje
 write.csv(dmy, file = file.path, row.names = FALSE)
+path.scores <- "01_tidy_data/scores.csv"
+write.csv(scores, file = path.scores, row.names = FALSE)
+path.quality <- "01_tidy_data/quality.csv"
+write.csv(quality, file = path.quality, row.names = F)
+
+# datafreimu tvarkymas ####
+#                DMY
+dmy <- dmy[-c(37, 44), ] # pasalinau elutes 37 ir 44 nes buvo tuscios
+dmy$year <- as.factor(dmy$year)
+dmy$use_year <- as.factor(dmy$use_year)
+dmy$total <- rowSums(dmy[, 3:6]) # padarau nauja stulpeli su visu metu derlium
+dmy[, 3:7] <- round(dmy[, 3:7], 2) # suapvalinau stulpelius 3-7 iki 2 vietu po kablelio
+
